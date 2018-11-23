a41da90e1c39d0fdb50e96b3e0b4504a58593b86
Solaz86 <solaz@misena.edu.co>
2018-11-23 00:23:39 -0500

    envio de variables al js de grafico

--- web/modules/custom/poll_graph/js/poll_graph.js
+++ web/modules/custom/poll_graph/js/poll_graph.js
@@ -1,105 +1,69 @@
+
 zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
 ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9","ee6b7db5b51705a13dc2339db3edaf6d"];
-// var tecnologia = drupalSettings.poll_graph.stats.tecnologia;
+var tecnologia = drupalSettings.poll_graph.stats.tecnologia;
+var innovacion = drupalSettings.poll_graph.stats.innovacion;
+var habilidades = drupalSettings.poll_graph.stats.habilidades;
+var promedio = drupalSettings.poll_graph.stats.;
 // alert(tecnologia);
-var myConfig = {
-  type : 'radar',
-  plot : {
-    aspect : 'area',
-    animation: {
-      effect:3,
-      sequence:1,
-      speed:700
-    }
-  },
-  scaleV : {
-    visible : false
-  },
-  scaleK : {
-    values : '3:1',
-    labels : ['Tecnologia','Habilidades blandas','Innovación'],
-    item : {
-      fontColor : '#607D8B',
-      backgroundColor : "white",
-      borderColor : "#aeaeae",
-      borderWidth : 1,
-      padding : '5 10',
-      borderRadius : 10
-    },
-    refLine : {
-      lineColor : '#c10000'
-    },
-    tick : {
-      lineColor : '#59869c',
-      lineWidth : 2,
-      lineStyle : 'dotted',
-      size : 20
-    },
-    guide : {
-      lineColor : "#607D8B",
-      lineStyle : 'solid',
-      alpha : 0.3,
-      backgroundColor : "#c5c5c5 #718eb4"
-    }
-  },
-  series : [
-    {
-      values : [59, 39, 38],
-      lineColor : 'red',
-      backgroundColor : 'white'
-    },
-    {
-      values : [50, 10, 100],
-      lineColor : 'blue',
-      backgroundColor : 'white'
-    },
-  ]
-};
-zingchart.render({
-  id : 'myChart',
-  data : myConfig,
-  height: '100%',
-  width: '100%'
-});
 
-
-(function (Drupal, drupalSettings) {
-  Drupal.behaviors.activeLinks = {
-    attach: function attach(context) {
-      var path = drupalSettings.path;
-      var queryString = JSON.stringify(path.currentQuery);
-      var querySelector = path.currentQuery ? '[data-drupal-link-query=\'' + queryString + '\']' : ':not([data-drupal-link-query])';
-      var originalSelectors = ['[data-drupal-link-system-path="' + path.currentPath + '"]'];
-      var selectors = void 0;
-
-      if (path.isFront) {
-        originalSelectors.push('[data-drupal-link-system-path="<front>"]');
-      }
-
-      selectors = [].concat(originalSelectors.map(function (selector) {
-        return selector + ':not([hreflang])';
-      }), originalSelectors.map(function (selector) {
-        return selector + '[hreflang="' + path.currentLanguage + '"]';
-      }));
-
-      selectors = selectors.map(function (current) {
-        return current + querySelector;
-      });
-
-      var activeLinks = context.querySelectorAll(selectors.join(','));
-      var il = activeLinks.length;
-      for (var i = 0; i < il; i++) {
-        activeLinks[i].classList.add('is-active');
-      }
-    },
-    detach: function detach(context, settings, trigger) {
-      if (trigger === 'unload') {
-        var activeLinks = context.querySelectorAll('[data-drupal-link-system-path].is-active');
-        var il = activeLinks.length;
-        for (var i = 0; i < il; i++) {
-          activeLinks[i].classList.remove('is-active');
+    var myConfig = {
+      type : 'radar',
+      plot : {
+        aspect : 'area',
+        animation: {
+          effect:3,
+          sequence:1,
+          speed:700
         }
-      }
-    }
-  };
-})(Drupal, drupalSettings);
+      },
+      scaleV : {
+        visible : false
+      },
+      scaleK : {
+        values : '3:1',
+        labels : ['Tecnologia','Habilidades blandas','Innovación'],
+        item : {
+          fontColor : '#607D8B',
+          backgroundColor : "white",
+          borderColor : "#aeaeae",
+          borderWidth : 1,
+          padding : '5 10',
+          borderRadius : 10
+        },
+        refLine : {
+          lineColor : '#c10000'
+        },
+        tick : {
+          lineColor : '#59869c',
+          lineWidth : 2,
+          lineStyle : 'dotted',
+          size : 20
+        },
+        guide : {
+          lineColor : "#607D8B",
+          lineStyle : 'solid',
+          alpha : 0.3,
+          backgroundColor : "#c5c5c5 #718eb4"
+        }
+      },
+      series : [
+        {
+          values : [promedio, promedio, promedio],
+          lineColor : 'red',
+          backgroundColor : 'white'
+        },
+        {
+          values : [tecnologia, habilidades, innovacion],
+          lineColor : 'blue',
+          backgroundColor : 'white'
+        },
+      ]
+    };
+    zingchart.render({
+      id : 'myChart',
+      data : myConfig,
+      height: '100%',
+      width: '100%'
+    });
+
